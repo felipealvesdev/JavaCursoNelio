8dfbdfa58926f996211e16a35cb5f3e604652c03
Felipe Alves <felipealexandre1408@gmail.com>
2023-10-19 18:30:30 -0300

    Adicionando exercicio resolvido 1

---
+++ exerciciosSecao11/exercicioResolvido1/entities/Department.java
@@ -0,0 +1,21 @@
+package exercicioResolvido1.entities;
+
+public class Department {
+
+    String name;
+
+    public Department(){
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ exerciciosSecao11/exercicioResolvido1/entities/enums/WorkerLevel.java
@@ -0,0 +1,8 @@
+package exercicioResolvido1.entities.enums;
+
+public enum WorkerLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
