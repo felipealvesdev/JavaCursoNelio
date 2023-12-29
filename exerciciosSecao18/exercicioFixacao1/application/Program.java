061ed5db8d7e465a1f23fae1174b191fce202e9e
Felipe Alves <felipealexandre1408@gmail.com>
2023-12-29 17:35:33 -0300

    Adicionando exercicio de fixacao 1

---
+++ exerciciosSecao18/exercicioFixacao1/application/Program.java
@@ -0,0 +1,45 @@
+package exercicioFixacao1.application;
+import exercicioFixacao1.entities.Contract;
+import exercicioFixacao1.entities.Installment;
+import exercicioFixacao1.services.ContractService;
+import exercicioFixacao1.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre os dados do contrato: ");
+        System.out.print("Numero: ");
+        int number = scan.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(scan.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = scan.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int n = scan.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(obj, n);
+
+        System.out.println("Parcelas:");
+
+        for(Installment installment : obj.getInstallments()) {
+            System.out.println(installment);
+        }
+
+
+        scan.close();
+    }
+}
